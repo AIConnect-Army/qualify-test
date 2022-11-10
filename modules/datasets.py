51023ea3489edf280fdff5577460af0b6046a70b
junhyoung <jh061993@gmail.com>
2022-11-10 10:40:03 +0900

    Applying data augmentation

--- modules/datasets.py
+++ modules/datasets.py
@@ -17,7 +17,7 @@ class SegDataset(Dataset):
         logger(obj): 로거 객체
         verbose(bool): 세부 로깅 여부
     """   
-    def __init__(self, paths, input_size, scaler, cache, mode='train', logger=None, verbose=False):
+    def __init__(self, paths, input_size, scaler, cache, mode='train', logger=None, verbose=False, transform=None):
         
         self.x_paths = paths
         self.y_paths = list(map(lambda x : x.replace('x', 'y'),self.x_paths))
@@ -27,6 +27,7 @@ class SegDataset(Dataset):
         self.verbose = verbose
         self.cache = cache
         self.mode = mode
+        self.transform = transform
 
 
     def __len__(self):
@@ -47,8 +48,8 @@ class SegDataset(Dataset):
             orig_size = x.shape
             x = cv2.cvtColor(x, cv2.COLOR_BGR2RGB)
             x = cv2.resize(x, self.input_size)
-            x = self.scaler(x)
-            x = np.transpose(x, (2, 0, 1))
+            # x = self.scaler(x)
+            # x = np.transpose(x, (2, 0, 1))
 
             self.cache[self.x_paths[id_]] = {"data":x, "size":orig_size}
 
@@ -60,9 +61,18 @@ class SegDataset(Dataset):
                 y = cv2.resize(y, self.input_size, interpolation=cv2.INTER_NEAREST)
                 self.cache[self.y_paths[id_]] = y
 
+            if self.transform:
+                x, y = np.expand_dims(x, axis=0), np.expand_dims(y, axis=0).transpose(1, 2, 0)
+                x, y = self.transform.transform(x, np.expand_dims(y, axis=0))
+                x, y = x[0], y[0][0]
+            else:
+                x = np.transpose(x, (2, 0, 1))
+
             return x, y, filename
 
         elif self.mode in ['test']:
+            x = self.scaler(x)
+            x = np.transpose(x, (2, 0, 1))
             return x, orig_size, filename
 
         else:
