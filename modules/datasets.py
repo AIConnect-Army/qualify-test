b89d6720ac81fdc64de36f548b9672debcbe02a5
jungsiroo <sh2298@naver.com>
2022-11-16 01:16:48 +0900

    For final : Refactoring

--- modules/datasets.py
+++ modules/datasets.py
@@ -16,15 +16,14 @@ class SegDataset(Dataset):
         logger(obj): 로거 객체
         verbose(bool): 세부 로깅 여부
     """   
-    def __init__(self, paths, input_size, scaler, cache=None, mode='train', logger=None, verbose=False):
-        
+    def __init__(self, paths, input_size, scaler, mode='train', logger=None, verbose=False):
+
         self.x_paths = paths
         self.y_paths = list(map(lambda x : x.replace('x', 'y'),self.x_paths))
         self.input_size = input_size
         self.scaler = scaler
         self.logger = logger
         self.verbose = verbose
-        self.cache = cache
         self.mode = mode
 
 
@@ -34,30 +33,18 @@ class SegDataset(Dataset):
     def __getitem__(self, id_: int):
 
         filename = os.path.basename(self.x_paths[id_]) # Get filename for logging
-        cache_data = self.cache.get(self.x_paths[id_], None)
+        x = cv2.imread(self.x_paths[id_], cv2.IMREAD_COLOR)
+        orig_size = x.shape
 
-        if cache_data:
-            x, orig_size = cache_data['data'],cache_data['size']
-        else:
-            x, orig_size = None, None
-
-        if x is None:
-            x = cv2.imread(self.x_paths[id_], cv2.IMREAD_COLOR)
-            orig_size = x.shape
-            x = cv2.cvtColor(x, cv2.COLOR_BGR2RGB)
-            x = cv2.resize(x, self.input_size)
+        x = cv2.cvtColor(x, cv2.COLOR_BGR2RGB)
+        x = cv2.resize(x, self.input_size)
+        if self.mode in ['train', 'valid']:
             x = self.scaler(x)
-            x = np.transpose(x, (2, 0, 1))
-
-            self.cache[self.x_paths[id_]] = {"data":x, "size":orig_size}
+        x = np.transpose(x, (2, 0, 1))
 
         if self.mode in ['train', 'valid']:
-            y = self.cache.get(self.y_paths[id_], None)
-
-            if y is None:
-                y = cv2.imread(self.y_paths[id_], cv2.IMREAD_GRAYSCALE)
-                y = cv2.resize(y, self.input_size, interpolation=cv2.INTER_NEAREST)
-                self.cache[self.y_paths[id_]] = y
+            y = cv2.imread(self.y_paths[id_], cv2.IMREAD_GRAYSCALE)
+            y = cv2.resize(y, self.input_size, interpolation=cv2.INTER_NEAREST)
 
             return x, y, filename
 
